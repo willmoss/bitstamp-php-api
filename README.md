08829d9d140d440e6a9f376aea53e7c71b4a2d9b
William Moss <willmoss26@gmail.com>
2013-10-05 01:17:07 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
-bitstamp-php-api
-================
+## Bitstamp PHP API
 
-PHP API for Bitstamp
+[<img src="https://bxmediaus.com/images/bx-media-logo.gif">](https://bxmediaus.com/)
+
+## INTRO
+
+This is a PHP API for Bitstamp's new API, released September 2013.
+
+To install, place in your PHP directory and include into your files.
+ 
+When you construct the method, you include your API_KEY, SECRET and CLIENT_ID, that can be found on the Bitstamp website.
+
+
+## FEATURES:
+
+bitstamp_query - Query any of the Bitstamp API methods
+
+ticker - Get current Bitstamp ticker
+
+eurusd - Get Bitstamp EUR/USD rate
+
+buyBTC/sellBTC - Buy or Sell BTC at current market price
+
+
+## EXAMPLE
+
+$bs = new Bitstamp("KEY","SECRET","CLIENT_ID");
+
+print_r($bs->ticker()); // show bid,ask & other price stats
+
+$bs->buyBTC(10); // buy 10 bitcoins at ask price
+
+$bs->bitstamp_query("buy", array('amount'=>'34','price'=>'90')); // place a limit order to buy 34 bitcoin at $90
+
+
+## Contributing
+
+Want to contribute? Great! 
+
+Just fork our code, make your changes, then let us know and we will incorporate.
+
+1. Fork it.
+2. Create a branch (`git checkout -b my_qror`)
+3. Commit your changes (`git commit -am "Added Snarkdown"`)
+4. Push to the branch (`git push origin my_qror`)
+5. Open a [Pull Request][1]
+6. Enjoy a refreshing Diet Coke and wait
+
+We encourage you to get in touch and tell us what you think of our code.
+
+We are constantly updating and improving our code. We hope it can be for the benefit of the entire community.
+
+If you want to chat to us, connect to our website: [bxmediaus.com][bxmediaus]
+
+
+[bxmediaus]: https://bxmediaus.com
---
+++ bitstamp.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * @package Bitstamp API
+ * @author https://bxmediaus.com - BX MEDIA - PHP + Bitcoin. We are ready to work on your next bitcoin project. Only high quality coding. https://bxmediaus.com
+ * @version 0.1
+ * @access public
+ * @license http://www.opensource.org/licenses/LGPL-3.0
+ */
+
+class Bitstamp
+{
+	private $key;
+	private $secret;
+	private $client_id;
+	public $redeemd;  // Redeemed code information
+	public $withdrew; // Withdrawal information
+	public $info;     // Result from getInfo()
+	public $ticker;   // Current ticker (getTicker())
+	public $eurusd;   // Current eur/usd
+	/**
+	 * Bitstamp::__construct()
+	 * Sets required key and secret to allow the script to function
+	 * @param Bitstamp API Key $key
+	 * @param Bitstamp Secret $secret
+	 * @return
+	 */
+	public function __construct($key, $secret, $client_id)
+	{
+		if (isset($secret) && isset($key) && isset($client_id))
+		{
+			$this->key = $key;
+			$this->secret = $secret;
+			$this->client_id = $client_id;
+		} else
+			die("NO KEY/SECRET/CLIENT ID");
+	}
+	/**
+	 * Bitstamp::bitstamp_query()
+	 * 
+	 * @param API Path $path
+	 * @param POST Data $req
+	 * @return Array containing data returned from the API path
+	 */
+	public function bitstamp_query($path, array $req = array())
+	{
+		// API settings
+		$key = $this->key;
+		
+		// generate a nonce as microtime, with as-string handling to avoid problems with 32bits systems
+		$mt = explode(' ', microtime());
+		$req['nonce'] = $mt[1] . substr($mt[0], 2, 6);
+		$req['key'] = $key;
+		$req['signature'] = $this->get_signature($req['nonce']);
+		
+		
+		// generate the POST data string
+		$post_data = http_build_query($req, '', '&');
+
+		// any extra headers
+		$headers = array();
+		
+		// our curl handle (initialize if required)
+		static $ch = null;
+		if (is_null($ch))
+		{
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_USERAGENT,
+				'Mozilla/4.0 (compatible; MtGox PHP client; ' . php_uname('s') . '; PHP/' .
+				phpversion() . ')');
+		}
+		curl_setopt($ch, CURLOPT_URL, 'https://www.bitstamp.net/api/' . $path .'/');
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+		// run the query
+		$res = curl_exec($ch);
+		if ($res === false)
+			throw new \Exception('Could not get reply: ' . curl_error($ch));
+		$dec = json_decode($res, true);
+		if (!$dec)
+			throw new \Exception('Invalid data received, please make sure connection is working and requested API exists');
+		return $dec;
+	}
+	
+	/**
+	 * Bitstamp::ticker()
+	 * Returns current ticker from Bitstamp
+	 * @return $ticker
+	 */
+	function ticker() {
+		$ticker = $this->bitstamp_query('ticker');
+		$this->ticker = $ticker; // Another variable to contain it.
+		return $ticker;
+	}
+	
+	/**
+	 * Bitstamp::eurusd()
+	 * Returns current EUR/USD rate from Bitstamp
+	 * @return $ticker
+	 */
+	function eurusd() {
+		$eurusd = $this->bitstamp_query('eur_usd');
+		$this->eurusd = $eurusd; // Another variable to contain it.
+		return $eurusd;
+	}
+
+	/**
+	* Bitstamp::buyBTC()
+	*
+	* @param float $amount
+	*/
+	function buyBTC($amount){
+	  
+	  if (!isset($ticker))
+	    $this->ticker();
+	  
+	  $ticker = $this->ticker;
+	  
+		return $this->bitstamp_query('buy', array('amount' => $amount, 'price' => $ticker['ask']));
+		
+	}
+
+	/**
+	* Bitstamp::sellBTC()
+	*
+	* @param float $amount
+	* @param float $price
+	* @param string $currency
+	*/
+	function sellBTC($amount){
+	  
+	  if (!isset($ticker))
+	    $this->ticker();
+	  
+	  $ticker = $this->ticker;
+	  
+		return $this->bitstamp_query('sell', array('amount' => $amount, 'price' => $ticker['bid']));
+		
+	}
+
+	
+	/**
+	* Bitstamp::get_signature()
+	* Compute bitstamp signature
+	* @param float $nonce
+	*/
+	private function get_signature($nonce)
+	{
+	  
+	  $message = $nonce.$this->client_id.$this->key;
+	  
+	  return strtoupper(hash_hmac('sha256', $message, $this->secret));
+	  
+	}
+}
+
